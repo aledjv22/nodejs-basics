1aeeb12f53fad8df1df2e3e6d896b9d5f6132eb9
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-21 23:43:10 -0300

    NodeJS basics

---
+++ concepts/environment.js
@@ -0,0 +1,5 @@
+let name = process.env.NAME || 'no name';
+let website = process.env.WEBSITE || 'no website';
+
+console.log('Hello ' + name);
+console.log('My website is ' + website);
---
+++ concepts/monothread.js
@@ -0,0 +1,9 @@
+console.log('Hello world');
+
+let i = 0;
+setInterval(() => {
+  console.log(i);
+  i++;
+}, 1000);
+
+console.log('Second instruction');

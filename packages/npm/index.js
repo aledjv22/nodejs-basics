37611422bdc814e330b7cab5618453dc3888eff5
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-09-26 00:06:32 -0300

    packages npm

---
+++ packages/npm/index.js
@@ -0,0 +1,17 @@
+// Import the 'is-odd' module
+const isOdd = require('is-odd');
+
+// Print to the console whether the number 3 is odd
+console.log(isOdd(3));
+
+// Print to the console whether the number 2 is odd
+console.log(isOdd(2));
+
+// Import the 'is-even' module
+const isEven = require('is-even');
+
+// Print to the console whether the number 3 is even
+console.log(isEven(3));
+
+// Print to the console whether the number 2 is even
+console.log(isEven(2));
